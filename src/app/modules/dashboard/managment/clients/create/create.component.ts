ea8078b68907d0f845705a52e8a47f2ab769c3c7
Isai Vargas <isaidvargas@hotmail.com>
2021-02-06 00:14:38 -0400

    feature: componente de creacion a marca, empleado y cliente

--- src/app/modules/dashboard/managment/clients/create/create.component.ts
+++ src/app/modules/dashboard/managment/clients/create/create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FieldConfig, status } from '../../../../../shared/components/models/field-config';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  status = status;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      id: this.fb.control(''),
+      name: this.fb.control('', Validators.required),
+      noDocument: this.fb.control('', Validators.required),
+      noCard: this.fb.control('', Validators.required),
+      creditLimit: this.fb.control('', Validators.required),
+      personType: this.fb.control('', Validators.required),
+      status: this.fb.control('', Validators.required),
+    });
   }
 
+  personType: FieldConfig = {
+    label: 'Tipo de Persona',
+    name: 'personType',
+    options: [
+      {
+        value: 'fisica',
+        viewValue: 'Física',
+      },
+      {
+        value: 'juridica',
+        viewValue: 'Jurídica',
+      }
+    ],
+  }
 }
--- src/app/modules/dashboard/managment/employees/create/create.component.ts
+++ src/app/modules/dashboard/managment/employees/create/create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FieldConfig, status } from '../../../../../shared/components/models/field-config';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  status = status;
+  todayDate = new Date();
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      id: this.fb.control(''),
+      name: this.fb.control('', Validators.required),
+      noDocument: this.fb.control('', Validators.required),
+      labour: this.fb.control('', Validators.required),
+      comission: this.fb.control('', Validators.required),
+      date: this.fb.control('', Validators.required),
+      status: this.fb.control('', Validators.required),
+    });
   }
 
+  labour: FieldConfig = {
+    label: 'Tanda de Labor',
+    name: 'labour',
+    options: [
+      {
+        value: 'Matutina',
+        viewValue: 'Matutina',
+      },
+      {
+        value: 'Vespertina',
+        viewValue: 'Vespertina',
+      },
+      {
+        value: 'Nocturna',
+        viewValue: 'Nocturna',
+      }
+    ],
+  }
 }
