fa2a4f4dc7b56ffc11a375f8a0fc89522fe651c1
Isai Vargas <isaidvargas@hotmail.com>
2021-02-06 00:14:08 -0400

    fix: mandar a dashboard cuando la ruta es vacia

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   {
     path: 'dashboard',
     loadChildren: () => import('./modules/dashboard/dashboard.module').then((m) => m.DashboardModule)
