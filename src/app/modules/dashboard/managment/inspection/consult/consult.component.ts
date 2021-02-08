8e6c349e6ba8efb5975bc2edf9297d62b0091159
Isai Vargas <isaidvargas@hotmail.com>
2021-02-08 00:49:53 -0400

    feature: traer data de inspección

--- src/app/modules/dashboard/managment/inspection/consult/consult.component.ts
+++ src/app/modules/dashboard/managment/inspection/consult/consult.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoaderService } from 'src/app/shared/services/loader.service';
+import { CrudActionsService } from '../../shared/services/crud-actions.service';
 
 @Component({
   selector: 'app-consult',
@@ -8,16 +10,74 @@ import { Component, OnInit } from '@angular/core';
 export class ConsultComponent implements OnInit {
 
   columns = {
-    definition: ['Id', 'vehicle', 'client', 'haveGrated', 'fuel', 'replacementTyre', 'jack', 'windowCrack', 'flTyre', 'frTyre', 'rlTyre', 'rrTyre', 'comentary', 'date', 'employee', 'status', 'actions'],
-    titles: ['Código', 'Vehículo', 'Cliente', 'Ralladuras', 'Cantidad de Combustible', 'Goma de Respuesta', 'Roturas de Cristal', 'Estado (Delantera izquierda)',
-      'Estado (Delantera derecha)', 'Estado (Trasera izquierda)', 'Estado (Trasera derecha)', 'Comentario', 'Fecha', 'Empleado', 'Estado', 'Acciones']
+    definition: ['Id', 'vehicle', 'client', 'haveGrated', 'haveReplacementTyre', 'haveWindowCrack', 'haveJack', 'fuel', 'flTyreStatus', 'frTyreStatus', 'rlTyreStatus', 'rrTyreStatus', 'date', 'comentary', 'employee', 'status', 'actions'],
+    titles: ['Código', 'Vehículo', 'Cliente', 'Ralladuras', 'Goma de Respuesta', 'Roturas de Cristal', 'Gato', 'Cantidad de Combustible', 'Estado (Delantera izquierda)',
+      'Estado (Delantera derecha)', 'Estado (Trasera izquierda)', 'Estado (Trasera derecha)', 'Fecha', 'Comentario', 'Empleado', 'Estado', 'Acciones']
   }
 
   data = []
+  employee = []
+  vehicle = []
+  client = []
 
-  constructor() { }
+  constructor(
+    private crudActions: CrudActionsService,
+    private loader: LoaderService
+  ) { }
 
   ngOnInit(): void {
+    this.loader.start();
+    this.crudActions.getData('Employees')
+      .subscribe(res => {
+        for (const vt of res) {
+          this.employee.push({
+            value: vt.EmployeeId, viewValue: vt.Name
+          });
+        }
+
+        this.crudActions.getData('Vehicles')
+          .subscribe(res => {
+            for (const vt of res) {
+              this.vehicle.push({
+                value: vt.VehicleId, viewValue: vt.Description
+              });
+            }
+
+            this.crudActions.getData('Clients')
+              .subscribe(res => {
+                for (const vt of res) {
+                  this.client.push({
+                    value: vt.ClientId, viewValue: vt.Name
+                  });
+                }
+
+                this.crudActions.getData('Inspections')
+                  .subscribe(res => {
+                    for (const vt of res) {
+                      console.log(vt);
+                      this.data.push({
+                        Id: vt.InspectionId, vehicle: this.getVehicle(vt.VehicleId), client: this.getClient(vt.ClientId), haveGrated: vt.HaveGrated, haveReplacementTyre: vt.HaveReplacementTyre, haveWindowCrack: vt.HaveWindowCrack, haveJack: vt.HaveJack, fuel: vt.Fuel,
+                        flTyreStatus: vt.FLTyreStatus, frTyreStatus: vt.FRTyreStatus, rlTyreStatus: vt.RLTyreStatus, rrTyreStatus: vt.RRTyreStatus, date: vt.Date,
+                        comentary: vt.Comment, employee: this.getEmployee(vt.EmployeeId), status: vt.Status
+                      });
+                    }
+                    console.log(this.data)
+                  });
+              });
+          });
+      });
+  }
+
+  getEmployee(value) {
+    return this.employee.find(element => element.value === value);
+  }
+
+  getClient(value) {
+    return this.client.find(element => element.value === value);
+  }
+
+  getVehicle(value) {
+    return this.vehicle.find(element => element.value === value);
   }
 
 }
